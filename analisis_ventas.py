798b40a943c73e8279f7d7af45e251b61b0dcb10
jjoseosorio <jjoseosorio97@gmail.com>
2025-02-28 14:31:56 -0300

    Se agrega archivo de analisis y grafico

--- analisis_ventas.py
+++ analisis_ventas.py
@@ -1,1 +1,25 @@
-print ("hola mundo")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargar los datos desde el archivo CSV
+df = pd.read_csv('ventas_productos.csv')
+
+# Calcular el precio total por producto
+df['Precio Total'] = df[' Cantidad'] * df[' Precio ']
+
+# Mostrar los primeros registros
+print(df.head())
+
+# Crear un gráfico de barras para visualizar el precio total por producto
+plt.bar(df['Producto'], df['Precio Total'])
+plt.xlabel('Producto')
+plt.ylabel('Precio Total')
+plt.title('Precio Total por Producto')
+plt.xticks(rotation=45)
+
+# Guardar el gráfico como PNG
+plt.savefig('grafico_precios.png')
+plt.show()
+
+
+
